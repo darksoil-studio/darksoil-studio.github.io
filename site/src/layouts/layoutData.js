e5af3327e7d6023b1a356bc8e036368a95edc678
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-09-19 16:00:09 +0200

    Initial pass

--- site/src/layouts/layoutData.js
+++ site/src/layouts/layoutData.js
@@ -1,17 +1,21 @@
-import { html } from 'lit';
+import { html } from "lit";
 
-import { PageTree } from '@rocket/engine';
+import { PageTree } from "@rocket/engine";
 
 export const pageTree = new PageTree();
-await pageTree.restore(new URL('../../pages/pageTreeData.rocketGenerated.json', import.meta.url));
+await pageTree.restore(
+  new URL("../../pages/pageTreeData.rocketGenerated.json", import.meta.url)
+);
 
 export const layoutData = {
   pageTree,
-  titleWrapperFn: title => title,
-  description: 'Welcome to the Rocket Spark Landing Page example',
-  siteName: 'Rocket',
+  titleWrapperFn: (title) => title,
+  description: "Welcome to the Rocket Spark Landing Page example",
+  siteName: "Rocket",
 
   head__150: html`<link rel="stylesheet" href="resolve:#src/css/page.css" />`,
+
+  header__100: html`<div style="display: inline-block"><rocket-social-link style="--primary-text-color: white; --primary-color: var(--theme-secondary); " name="Github" url="https://github.com/darksoil-studio"></rocket-social-link></div>`,
 
   footer__10: html`
     <rocket-content-area>
