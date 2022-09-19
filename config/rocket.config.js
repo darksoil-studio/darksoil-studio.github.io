d574d87e7cc297965e54ef47d2076e72ba840cbc
Guillem Cordoba <guillem.cordoba@gmail.com>
2022-09-19 16:00:30 +0200

    Updates

--- config/rocket.config.js
+++
@@ -1,8 +0,0 @@
-import { rocketSpark } from '@rocket/spark';
-
-export default /** @type {import('@rocket/cli').RocketCliOptions} */ ({
-  absoluteBaseUrl: 'http://localhost:8080',
-  longFileHeaderWidth: 100,
-  longFileHeaderComment: '// prettier-ignore',
-  presets: [rocketSpark()],
-});
