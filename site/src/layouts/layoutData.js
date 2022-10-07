814d58eac1997042a496e0744c49dc46146a5704
Viktor <zaunders@gmail.com>
2022-10-07 17:38:21 +0200

    general

--- site/src/layouts/layoutData.js
+++ site/src/layouts/layoutData.js
@@ -18,22 +18,25 @@ export const layoutData = {
   header__100: html`<div style="display: inline-block"><rocket-social-link style="--primary-text-color: white; --primary-color: var(--theme-secondary); " name="Github" url="https://github.com/darksoil-studio"></rocket-social-link></div>`,
 
   footer__10: html`
-    <rocket-content-area>
-      <rocket-columns>
-        <div>
-          <strong>Find Us</strong><br />
-          <br />
-          email:<br />
-          
-        </div>
-
-        <div class="legal">
-          <strong>More</strong> <br />
-          <a href="#">About</a><br />
-          <a href="#">Us</a><br />
-          <a href="#">Welcome</a><br />
-        </div>
-      </rocket-columns>
-    </rocket-content-area>
   `,
 };
+
+
+/*old footer
+<rocket-content-area>
+<rocket-columns>
+<div>
+  <strong>Find Us</strong><br />
+  <br />
+  email:<br />
+  
+</div>
+
+<div class="legal">
+  <strong>More</strong> <br />
+  <a href="#">About</a><br />
+  <a href="#">Us</a><br />
+  <a href="#">Welcome</a><br />
+</div>
+</rocket-columns>
+</rocket-content-area>*/
