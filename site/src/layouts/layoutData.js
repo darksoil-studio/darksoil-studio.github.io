3396a2316ffef08be1ac9497158aa6340d8c043a
Guillem Córdoba <guillem.cordoba@gmail.com>
2023-02-06 16:03:32 +0100

    Added icons on top

--- site/src/layouts/layoutData.js
+++ site/src/layouts/layoutData.js
@@ -6,21 +6,40 @@ export const pageTree = new PageTree();
 await pageTree.restore(
   new URL("../../pages/pageTreeData.rocketGenerated.json", import.meta.url)
 );
+import "../elements/custom-social-link.js";
 
 export const layoutData = {
   pageTree,
   titleWrapperFn: (title) => title,
-  description: "Welcome to the Rocket Spark Landing Page example",
-  siteName: "Rocket",
+  description: "Welcome to darksoil studio",
+  siteName: "darksoil studio",
 
   head__150: html`<link rel="stylesheet" href="resolve:#src/css/page.css" />`,
 
-  header__100: html`<div style="display: inline-block"><rocket-social-link style="--primary-text-color: white; --primary-color: var(--theme-secondary); " name="Github" url="https://github.com/darksoil-studio"></rocket-social-link></div>`,
+  header__100: html` <div style="display: inline-block; position: relative">
+    <div
+      style="display: flex; position: absolute; margin-top: -22px; justify-contents: center; align-items: center"
+    >
+      <rocket-social-link
+        style="--primary-text-color: white; --primary-color: var(white); "
+        name="Github"
+        url="https://github.com/darksoil-studio"
+      ></rocket-social-link>
+      <custom-social-link
+        style="--primary-text-color: white; --primary-color: var(--theme-secondary); "
+        name="OpenCollective"
+        url="https://opencollective.com/darksoil"
+      ></custom-social-link>
+      <custom-social-link
+        style="--primary-text-color: white; --primary-color: var(--theme-secondary); "
+        name="Ghost"
+        url="https://substack.com/profile/5969055-darksoil"
+      ></custom-social-link>
+    </div>
+  </div>`,
 
-  footer__10: html`
-  `,
+  footer__10: html``,
 };
-
 
 /*old footer
 <rocket-content-area>
