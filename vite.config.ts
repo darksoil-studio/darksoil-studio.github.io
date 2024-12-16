d9dd7ed23c8c85d245a7ce9a060bace48c6339eb
Viktor Zaunders <viktor@darksoil.studio>
2024-12-16 10:53:50 +0100

    hc-intro fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,22 @@
-import { defineConfig } from 'vite';
+/* import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: ''
 });
+*/
+
+import { resolve } from 'path' 
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        hc_intro: resolve(__dirname, 'hc-intro/index.html'),
+      },
+    },
+  },
+});
+
