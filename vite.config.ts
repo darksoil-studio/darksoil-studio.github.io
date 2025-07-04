93d117f28be95420dd5569d728aa1d46f31c3fe5
Viktor Zaunders <zaunders@gmail.com>
2025-07-04 13:24:08 +0200

    updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+/*import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: ''
 });
 
-/*
+
 import { resolve } from 'path' 
 import { defineConfig } from 'vite';
 
@@ -20,3 +20,19 @@ export default defineConfig({
   },
 });*/
 
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        dash: resolve(__dirname, 'dash-chat.html'),
+        corri: resolve(__dirname, 'corri.html'),
+        bouy: resolve(__dirname, 'bouy-os.html'),
+        // Add more pages here
+      }
+    }
+  }
+})
